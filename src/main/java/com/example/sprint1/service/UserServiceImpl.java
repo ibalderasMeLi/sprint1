fb3a134f750cf1634cb6b642dca73bb7426a8f61
andr4i <andreigonzalo@outlook.com>
2024-04-24 12:14:49 -0600

    Added Service Method getFollowedList
    The firm for method getFollowedList was added to the userService Interface as well as the implmentation. Also there were added the dtos for the response.

--- src/main/java/com/example/sprint1/service/UserServiceImpl.java
+++ src/main/java/com/example/sprint1/service/UserServiceImpl.java
@@ -1,8 +1,16 @@
 package com.example.sprint1.service;
 
+import com.example.sprint1.dto.FollowdUsersDto;
+import com.example.sprint1.dto.FollowedListDto;
+import com.example.sprint1.exception.NotFoundException;
+import com.example.sprint1.model.User;
 import com.example.sprint1.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements IUserService{
@@ -25,9 +33,33 @@ public class UserServiceImpl implements IUserService{
         return null;
     }
 
+    /**
+     * This method is used to get the list of users that the user follows and package it into a FollowedListDto object
+     * @param userId
+     * @return
+     */
     @Override
-    public Object getFollowedList(Integer userId) {
-        return null;
+    public FollowedListDto getFollowedList(Integer userId) {
+        // Get all users from repository
+        List<User> allUsers = userRepository.findAll();
+        // Select the user that matches with the id supplied
+        Optional<User> userSpecified = allUsers.stream().filter(user -> user.getId() == userId).findFirst();
+        if(userSpecified.isPresent()&&userSpecified.get().getFollowed()!=null){
+            // Get the list of users that the user follows
+            Set<Integer> followedList = userSpecified.get().getFollowed();
+            // Return the list of users that the user follows pa
+            List<FollowdUsersDto> followdUsersDtos = allUsers.stream().
+                    filter(user -> followedList.contains(user.getId())).
+                    map(user -> new FollowdUsersDto(user.getId(), user.getUser_name())).toList();
+            FollowedListDto followedListDto = new FollowedListDto();
+            followedListDto.setFollowed(followdUsersDtos);
+            followedListDto.setUser_id(userId);
+            followedListDto.setUser_name(userSpecified.get().getUser_name());
+            return followedListDto;
+        }
+        else{
+            throw new NotFoundException("User not Found");
+        }
     }
 
     @Override
@@ -37,6 +69,11 @@ public class UserServiceImpl implements IUserService{
 
     @Override
     public Object getFollowersOrdered(Integer userId, String order) {
+        return null;
+    }
+
+    @Override
+    public Object getFollowedOrdered(Integer userId, String order) {
         return null;
     }
 }
