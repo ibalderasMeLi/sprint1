0163e3b0002fb46fe3a5134482b9b62849d3f7af
yairvalde96 <164570090+yairvalde96@users.noreply.github.com>
2024-04-25 10:23:09 -0600

    Merge with all proyect

--- src/main/java/com/example/sprint1/controller/PostController.java
+++ src/main/java/com/example/sprint1/controller/PostController.java
@@ -14,33 +14,65 @@ public class PostController {
     @Autowired
     IPostService postService;
 
-    //US 0005
-    @PostMapping("/products/post")
-    public ResponseEntity<?> addPost(@RequestBody PostDto postDto){
-        return new ResponseEntity<>(postService.addPost(postDto) , HttpStatus.CREATED);
-    }
 
-    //US 0006 US 0009
-    @GetMapping("/followed/{userId}/list")
-    public ResponseEntity<?> followedList(@PathVariable Integer userId,@RequestParam(value = "order", required = false) String order){
-        return new ResponseEntity<>(postService.followedList(userId,order), HttpStatus.OK);
-    }
+        /**
+         * US 0005
+         * Create a new post
+         * @param postDto
+         * @return
+         */
+        @PostMapping("/products/post")
+        public ResponseEntity<?> addPost (@RequestBody PostDto postDto){
+            return new ResponseEntity<>(postService.addPost(postDto), HttpStatus.CREATED);
+        }
 
-    //US 0010:
-    @PostMapping("/promo-post")
-    public ResponseEntity<?> postPromo(@RequestBody PostDto postDto){
-        return new ResponseEntity<>(postService.postPromo(postDto), HttpStatus.CREATED);
-    }
 
-    //US 0011
-    @GetMapping("/promo-post/count")
-    public ResponseEntity<?> quantityPromo(@RequestParam Integer user_id){
-        return new ResponseEntity<>(postService.quantityPromo(user_id), HttpStatus.OK);
-    }
+        /**
+         * US 0006 US 0009
+         * Obtain a list of the publications made by the sellers that a user follows in the last two weeks
+         * Sort by ascending and descending date
+         * @param userId
+         * @param order
+         * @return
+         */
+        @GetMapping("/followed/{userId}/list")
+        public ResponseEntity<?> followedList (@PathVariable Integer
+        userId, @RequestParam(value = "order", required = false) String order){
+            return new ResponseEntity<>(postService.followedList(userId, order), HttpStatus.OK);
+        }
 
-    //US 0012
-    @GetMapping("/promo-post/list")
-    public ResponseEntity<?> getPromo(@RequestParam Integer user_id){
-        return new ResponseEntity<>(postService.getPromo(user_id), HttpStatus.OK);
+        /**
+         * US 0010
+         * Carry out the publication of a new promotional product
+         * @param postDto
+         * @return
+         */
+        @PostMapping("/promo-post")
+        public ResponseEntity<?> postPromo (@RequestBody PostDto postDto){
+            return new ResponseEntity<>(postService.postPromo(postDto), HttpStatus.CREATED);
+        }
+
+
+        /**
+         * US 0011
+         * Obtain the number of products on promotion from a certain seller
+         * @param user_id
+         * @return
+         */
+        @GetMapping("/promo-post/count")
+        public ResponseEntity<?> quantityPromo (@RequestParam Integer user_id){
+            return new ResponseEntity<>(postService.quantityPromo(user_id), HttpStatus.OK);
+        }
+
+
+        /**
+         * US 0012
+         * Obtain a list of all the products on promotion from a certain seller
+         * @param user_id
+         * @return
+         */
+        @GetMapping("/promo-post/list")
+        public ResponseEntity<?> getPromo (@RequestParam Integer user_id){
+            return new ResponseEntity<>(postService.getPromo(user_id), HttpStatus.OK);
+        }
     }
-}
--- src/main/java/com/example/sprint1/controller/UserController.java
+++ src/main/java/com/example/sprint1/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.sprint1.controller;
 
+import com.example.sprint1.exception.NotFoundException;
 import com.example.sprint1.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,34 +15,93 @@ public class UserController {
     @Autowired
     IUserService userService;
 
-    //US 0001
-    @PostMapping("/{userID}/follow/{userIdToFollow}")
+    /**
+     * US 001
+     * Endpoint that implements that follows a certain seller
+     * @param userID
+     * @param userIdToFollow
+     * @return
+     */
+    @PutMapping("/{userID}/follow/{userIdToFollow}")
     public ResponseEntity<?> postNewFollower(@PathVariable Integer userID, @PathVariable Integer userIdToFollow){
-        return new ResponseEntity<>(userService.addFollower(userID,userIdToFollow), HttpStatus.CREATED);
+        userService.addFollower(userID,userIdToFollow);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    //US 0002
+    /**
+     * US 0002
+     * Gets the quantity of users that follow a seller
+     * @param userId
+     * @return
+     */
     @GetMapping("/{userId}/followers/count")
     public ResponseEntity<?> getFollowersCount(@PathVariable Integer userId){
         return new ResponseEntity<>(userService.getFollowerCount(userId),HttpStatus.OK);
     }
 
-    //US 0003, US 0008
+
+
+    /*
+     * This method is used to get the list of followers that the user have and package it into a FolloweRListDto object
+     * @param userId - The id of the user
+     * @return - A FollowerListDto object that contains the list of followers that the user have
+     */
     @GetMapping("/{userId}/followers/list")
-    public ResponseEntity<?> getFollowerList(@PathVariable Integer userId,@RequestParam String order){
-        return new ResponseEntity<>(userService.getFollowerList(userId,order),HttpStatus.OK);
+    public ResponseEntity<?> getFollowerList(@PathVariable Integer userId, @RequestParam(required = false) String order){
+
+        // Validation on whether the list will have any order
+        if (order == null || order.isEmpty()){
+            return new ResponseEntity<>(userService.getFollowerList(userId, order),HttpStatus.OK);
+        }
+        if (order.equals("name_asc") || order.equals("name_desc")){
+            return new ResponseEntity<>(userService.getFollowersOrdered(userId, order),HttpStatus.OK);
+        }
+        else {
+            throw new NotFoundException("Query params not matching any case");
+
+        }
     }
 
     //US 0004 US, US 0008
+
+    /**
+     * US 0004, US 0008
+     * List of all the sellers that follow some user
+     * Sort alphabetically, ascending and descending
+     * @param userId
+     * @param order
+     * @return
+     */
     @GetMapping("/{userId}/followed/list")
-    public ResponseEntity<?> getFollowedList(@PathVariable Integer userId,@RequestParam String order){
-        return new ResponseEntity<>(userService.getFollowedList(userId,order),HttpStatus.OK);
+    public ResponseEntity<?> getFollowedList(@PathVariable Integer userId, @RequestParam(value = "order", required = false) String order){
+        if (order == null){
+            return new ResponseEntity<>(userService.getFollowedList(userId), HttpStatus.OK);
+        }
+        if(order.equals("name_asc") || order.equals("name_desc")){
+            return new ResponseEntity<>(userService.getFollowedOrdered(userId, order), HttpStatus.OK);
+        }
+        else {
+            throw new NotFoundException("Query params not matching any case");
+        }
     }
 
     //US 0007
+
+    /**
+     * US 0007
+     * Unfollows a user (userId unfollows userIdToUnfollow)
+     * @param userId
+     * @param userIdToUnfollow
+     * @return
+     */
     @DeleteMapping("/{userId}/unfollow/{userIdToUnfollow}")
     public ResponseEntity<?> setUnfollow(@PathVariable Integer userId,@PathVariable Integer userIdToUnfollow){
         return new ResponseEntity<>(userService.setUnfollow(userId,userIdToUnfollow),HttpStatus.NO_CONTENT);
     }
-
+          
+    //TODO quitar el ultimo método
+    @GetMapping("{userId}/followers/list-ordered")
+    public ResponseEntity<?> getFollowerListOrderByName(@PathVariable Integer userId,@RequestParam String order){
+        return new ResponseEntity<>(userService.getFollowersOrdered(userId,order),HttpStatus.OK);
+    }
 }
